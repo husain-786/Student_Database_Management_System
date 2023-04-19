195e7e940730cf4b43b43b691cd7641a47520ceb
husain-786 <63996307+husain-786@users.noreply.github.com>
2023-04-19 20:30:26 +0530

    Add files via upload

---
+++ src/student_database_management_system/Student_DataBase_Management_System.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student_database_management_system;
+
+/**
+ *
+ * @author Shahid Husain
+ */
+public class Student_DataBase_Management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new SplashScreen().setVisible(true);
+    }
+    
+}
